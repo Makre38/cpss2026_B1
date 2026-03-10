1d86d6e2cabe424aa400bf3a10682a7d94d084c3
Makre38 <makre38@vivaldi.net>
2026-03-10 16:16:01 +0900

    Demonstrate Metropolis sweep in script

--- generate_lattice.jl
+++ generate_lattice.jl
@@ -3,6 +3,7 @@ include("src/CPSS2026B1.jl")
 using .CPSS2026B1
 
 const PARAMS_FILE = "params.dat"
+const BETA = 0.4
 
 lattice = generate_lattice_from_file(PARAMS_FILE)
 
@@ -13,3 +14,8 @@ for row in eachrow(lattice)
 end
 println("magnetization = ", magnetization(lattice))
 println("energy = ", energy(lattice))
+
+accepted = metropolis_sweep!(lattice; β = BETA)
+println("accepted flips = ", accepted)
+println("updated magnetization = ", magnetization(lattice))
+println("updated energy = ", energy(lattice))

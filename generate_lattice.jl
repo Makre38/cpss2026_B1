1f55395a85a4c5413774b064e1b991f1d64c3a0e
Makre38 <makre38@vivaldi.net>
2026-03-10 16:28:14 +0900

    Initialize simulation from params file

--- generate_lattice.jl
+++ generate_lattice.jl
@@ -3,19 +3,23 @@ include("src/CPSS2026B1.jl")
 using .CPSS2026B1
 
 const PARAMS_FILE = "params.dat"
-const BETA = 0.4
 
-lattice = generate_lattice_from_file(PARAMS_FILE)
+params = read_parameters(PARAMS_FILE)
+beta = 1 / params.temperature
 
-println("L = ", size(lattice, 1))
+lattice = generate_lattice(params.L)
+
+println("L = ", params.L)
+println("T = ", params.temperature)
+println("J = ", params.coupling)
 println("lattice =")
 for row in eachrow(lattice)
     println(join(row, " "))
 end
 println("magnetization = ", magnetization(lattice))
-println("energy = ", energy(lattice))
+println("energy = ", energy(lattice; J = params.coupling))
 
-accepted = metropolis_sweep!(lattice; β = BETA)
+accepted = metropolis_sweep!(lattice; β = beta, J = params.coupling)
 println("accepted flips = ", accepted)
 println("updated magnetization = ", magnetization(lattice))
-println("updated energy = ", energy(lattice))
+println("updated energy = ", energy(lattice; J = params.coupling))
